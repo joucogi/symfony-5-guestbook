95ab67dce189aa4e9a67940e0615861795730f9b
Joel Coll Costa <joel.coll@gmail.com>
2021-01-02 19:31:51 +0100

    Create header in base template showing all conferences.

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -36,13 +36,15 @@ class ConferenceController extends AbstractController
     public function show(
         Request $request,
         Conference $conference,
-        CommentRepository $repository
+        CommentRepository $commentRepository,
+        ConferenceRepository $conferenceRepository
     ): Response {
         $offset = max(0, $request->query->getInt('offset', 0));
-        $paginator = $repository->getCommentPaginator($conference, $offset);
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return new Response($this->twig->render(
             'conference/show.html.twig', [
+                'conferences' => $conferenceRepository->findAll(),
                 'conference' => $conference,
                 'comments' => $paginator,
                 'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
