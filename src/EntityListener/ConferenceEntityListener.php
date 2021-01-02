0b447e6c50955af87459efe055c955700835301e
Joel Coll Costa <joel.coll@gmail.com>
2021-01-02 20:48:23 +0100

    Configuring a Service in the Container

---
+++ src/EntityListener/ConferenceEntityListener.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EntityListener;
+
+use App\Entity\Conference;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+final class ConferenceEntityListener {
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger) {
+        $this->slugger = $slugger;
+    }
+
+    public function prePersist(Conference $conference, LifecycleEventArgs $event) {
+        $conference->computeSlug($this->slugger);
+    }
+
+    public function preUpdate(Conference $conference, LifecycleEventArgs $event) {
+        $conference->computeSlug($this->slugger);
+    }
+}
