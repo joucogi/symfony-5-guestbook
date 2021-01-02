e84b72346544b0e4989446701dc373ddd8a7862b
Joel Coll Costa <joel.coll@gmail.com>
2021-01-02 18:58:35 +0100

    Paginating the comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,6 +6,7 @@ use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -26,15 +27,21 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conferences/{id}", name="conference")
      */
-    public function show(Environment $twig, Conference $conference, CommentRepository $repository): Response {
+    public function show(
+        Request $request,
+        Environment $twig,
+        Conference $conference,
+        CommentRepository $repository
+    ): Response {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $repository->getCommentPaginator($conference, $offset);
+
         return new Response($twig->render(
             'conference/show.html.twig', [
                 'conference' => $conference,
-                'comments' => $repository->findBy([
-                    'conference' => $conference
-                ], [
-                    'createdAt' => 'DESC'
-                ])
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
             ]
         ));
     }
