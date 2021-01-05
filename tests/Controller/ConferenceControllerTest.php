131a51c5ba7acd916e81c8e84d577f05aea24be7
Joel Coll Costa <joel.coll@gmail.com>
2021-01-05 17:53:26 +0100

    Crawling a website in functional tests

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use PHPUnit\Exception;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ConferenceControllerTest extends WebTestCase {
@@ -11,5 +12,20 @@ class ConferenceControllerTest extends WebTestCase {
 
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback');
+    }
+
+    public function testConferencePage() {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+        //$client->click($crawler->filter('h4 + p a')->link());
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2019');
+        $this->assertSelectorExists('div:contains("There are 1 comments")');
     }
 }
