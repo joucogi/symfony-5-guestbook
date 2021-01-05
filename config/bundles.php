63bfbbc98c90c122d958077c0fc674ea211d8160
Joel Coll Costa <joel.coll@gmail.com>
2021-01-05 17:21:28 +0100

    Defining Fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
