1b8dccc6f8b88ce7ad15d4538a9a7e2c7aae1449
Joel Coll Costa <joel.coll@gmail.com>
2021-01-10 11:31:31 +0100

    Avoiding SQL Request with ESI

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Entity\Conference;
 use App\Form\CommentFormType;
 use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
@@ -32,6 +33,18 @@ class ConferenceController extends AbstractController {
      */
     public function index(): Response {
         $response = new Response($this->twig->render('conference/index.html.twig'));
+        $response->setSharedMaxAge(3600);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/conference/header", name="conference_header")
+     */
+    public function conferenceHeader(ConferenceRepository $repository): Response {
+        $response = new Response($this->twig->render('conference/header.html.twig', [
+            'conferences' => $repository->findAll(),
+        ]));
         $response->setSharedMaxAge(3600);
 
         return $response;
