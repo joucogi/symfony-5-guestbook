e6075a452da76a08038eb105f9780efdfe266699
Joel Coll Costa <joel.coll@gmail.com>
2021-01-13 00:07:28 +0100

    Notifying Admins by Email

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -6,14 +6,15 @@ namespace App\MessageHandler;
 
 use App\ImageOptimizer;
 use App\Message\CommentMessage;
+use App\Notification\CommentReviewNotification;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bridge\Twig\Mime\NotificationEmail;
-use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Notifier\NotifierInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
 
 final class CommentMessageHandler implements MessageHandlerInterface {
@@ -22,9 +23,8 @@ final class CommentMessageHandler implements MessageHandlerInterface {
     private $commentRepository;
     private $bus;
     private $workflow;
-    private $mailer;
+    private $notifier;
     private $imageOptimizer;
-    private $adminEmail;
     private $photoDir;
     private $logger;
 
@@ -34,9 +34,8 @@ final class CommentMessageHandler implements MessageHandlerInterface {
         CommentRepository $commentRepository,
         MessageBusInterface $bus,
         WorkflowInterface $commentStateMachine,
-        MailerInterface $mailer,
+        NotifierInterface $notifier,
         ImageOptimizer $imageOptimizer,
-        string $adminEmail,
         string $photoDir,
         LoggerInterface $logger = null
     ) {
@@ -46,8 +45,7 @@ final class CommentMessageHandler implements MessageHandlerInterface {
         $this->bus               = $bus;
         $this->workflow          = $commentStateMachine;
         $this->logger            = $logger;
-        $this->mailer            = $mailer;
-        $this->adminEmail        = $adminEmail;
+        $this->notifier          = $notifier;
         $this->imageOptimizer    = $imageOptimizer;
         $this->photoDir          = $photoDir;
     }
@@ -72,12 +70,9 @@ final class CommentMessageHandler implements MessageHandlerInterface {
             $this->bus->dispatch($message);
 
         } else if ($this->workflow->can($comment, 'publish') || $this->workflow->can($comment, 'publish_ham')) {
-            $this->mailer->send(
-                (new NotificationEmail())->subject('New comment posted')
-                                         ->htmlTemplate('emails/comment_notification.html.twig')
-                                         ->from($this->adminEmail)
-                                         ->to($this->adminEmail)
-                                         ->context(['comment' => $comment])
+            $this->notifier->send(
+                new CommentReviewNotification($comment),
+                ...$this->notifier->getAdminRecipients()
             );
         } else if ($this->workflow->can($comment, 'optimize')) {
             if ($comment->getPhotoFilename()) {
