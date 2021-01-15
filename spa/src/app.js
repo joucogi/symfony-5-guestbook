8486ccff30297e513eb26758aca254ffc60212e8
Joel Coll Costa <joel.coll@gmail.com>
2021-01-15 18:27:08 +0100

    Adding a Router to handle State

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,9 +1,21 @@
 import {h, render} from 'preact';
+import {Router, Link} from 'preact-router';
+
+import Home       from './pages/home';
+import Conference from './pages/conference';
 
 function App() {
   return (
     <div>
-      Hello world!
+      <header>
+        <Link href="/">Home</Link>
+        <br />
+        <Link href="/conferences/amsterdam-2019">Amsterdam 2019</Link>
+      </header>
+      <Router>
+        <Home path="/" />
+        <Conference path="/conferences/:slug" />
+      </Router>
     </div>
   )
 }
